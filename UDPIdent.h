96cfd2a2b1ba34874f9550f07eacd98ae3be78a9
Seimen <code@seimenadventure.de>
2022-12-29 23:27:40 +0100

    first commit

---
+++ UDPIdent.h
@@ -0,0 +1,27 @@
+#ifndef UDPIdent_h
+#define UDPIdent_h
+#include <Arduino.h>
+#include <WiFiUdp.h>
+#include <IPAddress.h>
+
+#define UDPPORT 4210
+
+namespace UDPIdent
+{
+    void begin();
+
+    void end();
+
+    bool pair(IPAddress &with);
+
+    bool discovered();
+
+    void ackwnoledge();
+
+    void broadcast();
+
+    bool identify(IPAddress &identifiedIP);
+
+};
+
+#endif
